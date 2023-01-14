9f13064b71aac4a1a9999324ce46f529636f43de
Уксус <timminski22@gmail.com>
2023-01-14 15:36:39 +0300

    привет на новом проекте

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-import time
-
-name_user = input(str("Введмте имя"))
 
 
-def cool(name_user):
-    if name_user == "Катя":
-        print("Добро пожаловать!")
-    if name_user == "Леша":
-        print("Добро пожаловать!")
-#hgjfhgj
 
-cool(name_user)
 
+
+print('hello world')
 
 
