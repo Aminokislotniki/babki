1075301035858736589bdeb1df071ac33c5213a7
Уксус <timminski22@gmail.com>
2023-01-14 14:51:48 +0300

    привет на новом проекте

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import time
 
-list = ['best']
-def cool():
-    for Kate in list:
-        if Kate == 'best':
-            print("Добро пожаловать!")
+name_user = input(str("Введмте имя"))
 
 
+def cool(name_user):
+    if name_user == "Катя":
+        print("Добро пожаловать!")
+    if name_user == "Леша":
+        print("Добро пожаловать!")
 
 
-print('hello')
+cool(name_user)
+
+
 
