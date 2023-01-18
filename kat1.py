ea2e5c5e494473b018477f40a55d09d0b548c529
Kateryna <volka2003@tut.by>
2023-01-18 14:51:38 +0300

    k

---
+++ kat1.py
@@ -0,0 +1,17 @@
+import requests
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import InputMediaPhoto
+import gspread
+import time
+from fuzzywuzzy import process
+
+bot = telebot.TeleBot('5683069905:AAGVpQBnaKoilz2UYWK1Ug3XoAENmDsTUyc');
+
+
+def dt(s):
+    s = s[1:]
+    return s
+
+def fs(st):
+    return(st[0])
