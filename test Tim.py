7b21730daa857663628f598d96444b0f3357f57f
Уксус <timminski22@gmail.com>
2023-01-18 21:26:18 +0300

    #

---
+++ test Tim.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+
+
+a = {1:"thj"}
+# os.mkdir("folder")
+# os.rmdir("folder")
+# open('lots/'"3.json", "w")
+os.remove('lots/'"text.json")
+# os.remove("text.json")
+
+# text_file = open('lots/'"text.json", "w")
+# text_file.write()
